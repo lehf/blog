fea893013f34284595cbbcbfccabca41478b010b
lehf <1764129339@qq.com>
2017-06-10 09:01:20 +0800

    edit-password

--- db/migrate/20170609093645_create_users.rb
+++ db/migrate/20170609093645_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password_disget
+      t.string :password_digest
       t.boolean :is_admin
 
       t.timestamps null: false
